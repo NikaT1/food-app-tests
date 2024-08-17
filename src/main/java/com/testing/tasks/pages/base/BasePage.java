ceb1cc08d35c4452d1d1fc7ca43f1094e2d4de08
nika <troynikv@gmail.com>
2024-08-17 12:57:39 +0300

    Add base page with page factory

--- src/main/java/com/testing/tasks/pages/base/BasePage.java
+++ src/main/java/com/testing/tasks/pages/base/BasePage.java
@@ -1,4 +1,20 @@
 package com.testing.tasks.pages.base;
 
+import com.testing.tasks.managers.DriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+
+    protected DriverManager driverManager = DriverManager.getInstance();
+
+    public BasePage() {
+        PageFactory.initElements(driverManager.getDriver(), this);
+    }
+
 }
