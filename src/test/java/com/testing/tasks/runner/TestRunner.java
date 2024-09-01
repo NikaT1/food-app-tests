8c9f360e2cd6efc51d786d6512bad845b502ec6f
nika <troynikv@gmail.com>
2024-09-01 13:34:26 +0300

    Add tests filters ability

--- src/test/java/com/testing/tasks/runner/TestRunner.java
+++ src/test/java/com/testing/tasks/runner/TestRunner.java
@@ -9,7 +9,6 @@ import static io.cucumber.core.options.Constants.*;
 @SelectClasspathResource("com.testing.tasks")
 @ConfigurationParameters({
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"),
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.testing.tasks.steps")
 })
 public class TestRunner {
